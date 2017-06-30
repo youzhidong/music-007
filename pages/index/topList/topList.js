3e01b5c0c51339b01c48cf06ab07d10ee032cee9
Your Name <you@example.com>
2017-06-30 18:02:09 +0800

    2

--- pages/index/topList/topList.js
+++ pages/index/topList/topList.js
@@ -7,18 +7,32 @@ Page({
   onLoad:function(opations){
     //console.log(opations.id);
     var that = this;
-    var id = opations.id
+    var id = opations.id  //获取ID
     util.getTopListD(id,function(data){
       
       that.setData({
-        topinfo: data.topinfo,
-        songlist: data.songlist,
-        update_time: data.update_time
+        topinfo: data.topinfo, //图片等等
+        songlist: data.songlist, //列表
+        update_time: data.update_time, //时间
+        deBackgroundColor: that.dealColor(data.color) //背景颜色
+        
       });
-      console.log(that)
+      //console.log(that)
     });
-    // util.getTopListD(function(id,data){
-      
-    // })
+
+  },
+  dealColor: function (rgb) {
+    if (!rgb) { return; }
+    let r = (rgb & 0x00ff0000) >> 16,
+      g = (rgb & 0x0000ff00) >> 8,
+      b = (rgb & 0x000000ff);
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+  },
+  playsongTap:function(ev){
+    var id = ev.currentTarget.dataset.xid;
+    console.log(id)
+    wx.navigateTo({
+      url: '../../playsong/playsong?id=' + id,
+    })
   }
 })
